4309b183713fffece50abceee968e3a525d3da8c
Hawry <hawry@hawry.net>
2016-05-03 11:35:14 +0200

    lab2 klar

--- lab2/loopseconds.c
+++ lab2/loopseconds.c
@@ -10,6 +10,8 @@
 #include <time.h>
 #include <string.h>
 
+volatile int itr_proceed = 0;
+
 typedef struct sigaction sigact;
 
 void handle_usr(int s) {
@@ -20,28 +22,37 @@ void sig(int s) {
   printf("sigint received\r\n");
 }
 
+void handle_alarm(int s) {
+  // printf("handle alarm called!\r\n");
+  itr_proceed = 0;
+}
 
 int main() {
   printf("%d\r\n",getpid());
-  sigset_t sig_block, sig_handle;
+  sigset_t sig_block, sig_handle; //, sig_alarm;
   sigfillset(&sig_block);
   sigemptyset(&sig_handle);
+  // sigemptyset(&sig_alarm);
 
   sigaddset(&sig_handle,SIGUSR1);
+  // sigaddset(&sig_alarm,SIGALRM);
 
-  sigact sig_int, sig_usr;
+  sigact sig_int, sig_usr, sig_alrm;
   sig_int.sa_handler = sig;
   sig_int.sa_flags = 0;
   sig_usr.sa_handler = handle_usr;
   sig_usr.sa_flags = 0;
+  sig_alrm.sa_handler = handle_alarm;
+  sig_alrm.sa_flags = 0;
 
   sigaction(SIGUSR1,&sig_usr,0);
   sigaction(SIGINT,&sig_int,0);
+  sigaction(SIGALRM,&sig_alrm,0);
 
   sigprocmask(SIG_BLOCK, &sig_block,0);
   sigprocmask(SIG_UNBLOCK,&sig_handle,0);
 
-  time_t endTime = time(NULL) + 10;
+  time_t endTime = time(NULL) + 5;
   while(time(NULL)<endTime) {}
   sigset_t pend;
   sigpending(&pend);
@@ -50,5 +61,16 @@ int main() {
     if (sigismember(&pend, i))
       printf("%d (%s)\r\n",i,strsignal(i));
   }
+  sigprocmask(SIG_UNBLOCK,&sig_block,0);
+
+  printf("Starting iteration benchmark\r\n");
+  int itr_time = 2;
+  int itr_count = 0;
+  itr_proceed = 1;
+  alarm(itr_time);
+  while(itr_proceed)
+    ++itr_count;
+  printf("performed %d iterations in %d seconds\r\n",itr_count,itr_time);
+
   printf("loop done\r\n");
 }
